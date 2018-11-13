56618321b89a79f333ac39a835242a94b2d3bc9e
Adam Fowler <adamfowleruk@gmail.com>
2018-11-13 22:34:18 +0000

    Up to compilation step

--- build-python.sh
+++ build-python.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
-H=/home/system/sources/haiku/headers
+H=/home/system/develop/headers/os
 L=/home/system/lib
+P=/boot/system/develop/headers/python2.7
+PLIB=/boot/system/lib/libpython2.7.so.1.0
+HLIBS=-L/boot/system/lib/libbe.so
 mkdir -p generated/swig-python
 cd generated/swig-python
-swig -c++ -python -I$H -outcurrentdir -v ../../swig/haiku-swig-python.i 
-g++ haiku-swig-python_wrap.cxx -I$H
-#g++ -shared -L$L/libbe.so haiku-swig-python.c haiku-swig-python,so
 
+echo "---------------------------------"
+echo "Creating Haiku Python C++ wrapper"
+echo "---------------------------------"
+swig -c++ -python -I$H -I$P -outcurrentdir -v -macroerrors ../../swig/haiku-swig-python.i 
+
+
+echo "----------------------------------"
+echo "Compiling Haiku Python C++ wrapper"
+echo "----------------------------------"
+g++ haiku-swig-python_wrap.cxx -c -std=c++11 -fpermissive  -U$H/app -I$H/support -I$P -L$PLIB $HLIBS
+#g++ -shared -L$L/libbe.so -L$PLIB haikuswigpython_wrap,so
+
+echo "----------------------------------"
+echo "Done."
+echo "----------------------------------"
